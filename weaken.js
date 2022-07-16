0519977b2295cf448bf77b3c5a0325c45b67f049
steve2602 <60362374+steve2602@users.noreply.github.com>
2022-07-16 13:53:18 +1000

    Create weaken.js

---
+++ weaken.js
@@ -0,0 +1,11 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+	var target = ns.args[0];
+	var sTime = ns.args[1];
+
+	if (sTime > 0) {
+		await ns.sleep(sTime);
+	}
+
+	await ns.weaken(target);
+}
